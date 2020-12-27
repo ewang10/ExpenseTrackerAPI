2e555a935e7b4ad6fb03d6c8705f390631c51b80
ewang10 <ericwang27759@gmail.com>
2020-12-27 15:31:57 -0800

    init commit to make Expense restful

--- src/main/java/com/ewang/ExpenseTrackerAPI/controller/ExpenseController.java
+++ src/main/java/com/ewang/ExpenseTrackerAPI/controller/ExpenseController.java
@@ -3,50 +3,78 @@ package com.ewang.ExpenseTrackerAPI.controller;
 import com.ewang.ExpenseTrackerAPI.domain.Expense;
 import com.ewang.ExpenseTrackerAPI.exception.ExpenseNotFoundException;
 import com.ewang.ExpenseTrackerAPI.repository.ExpenseRepository;
+import com.ewang.ExpenseTrackerAPI.util.ExpenseModelAssembler;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 //RestController indicates that the data returned by each method will be
 //written straight into the response body instead of rendering a template.
 @RestController
 public class ExpenseController {
     private final ExpenseRepository repository;
-    ExpenseController(ExpenseRepository repository) {
+    private final ExpenseModelAssembler assembler;
+    ExpenseController(ExpenseRepository repository, ExpenseModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
+    //CollectionModel<> is another Spring HATEOAS container. It's aimed at encapsulating
+    //collections of resources—instead of a single resource entity, like EntityModel<>
+    //CollectionModel<> lets you include links as well
     @GetMapping("/expenses")
-    List<Expense> all() {
-        return repository.findAll();
+    public CollectionModel<EntityModel<Expense>> all() {
+        List<EntityModel<Expense>> expenses = repository.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(expenses,
+                linkTo(methodOn(ExpenseController.class).all()).withSelfRel());
     }
 
+    //EntityModel<T> is a generic container from Spring HATEOAS that includes not only the data but a collection of links.
     @GetMapping("/expenses/{id}")
-    Expense getExpenseById(@PathVariable Long id) {
-        return repository.findById(id)
+    public EntityModel<Expense> getExpenseById(@PathVariable Long id) {
+        Expense expense = repository.findById(id)
                 .orElseThrow(() -> new ExpenseNotFoundException(id));
+        return assembler.toModel(expense);
     }
 
     @GetMapping("/expenses/year/{year}")
-    List<Expense> getExpensesByYear(@PathVariable int year) {
-        return repository.findByYear(year);
+    CollectionModel<EntityModel<Expense>> getExpensesByYear(@PathVariable int year) {
+        List<EntityModel<Expense>> expensesByYear = repository.findByYear(year).stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(expensesByYear,
+                linkTo(methodOn(ExpenseController.class).getExpensesByYear(year)).withSelfRel());
     }
 
     @GetMapping("/expenses/year/{year}/month/{month}")
-    List<Expense> getExpensesByYearAndMonth(@PathVariable int year, @PathVariable String month) {
+    CollectionModel<EntityModel<Expense>> getExpensesByYearAndMonth(@PathVariable int year, @PathVariable String month) {
         if ("All".equals(month)) {
-            return repository.findByYear(year);
+            return this.getExpensesByYear(year);
         }
-        return repository.findByYearAndMonth(year, month);
+        List<EntityModel<Expense>> expensesByYearAndMonth = repository.findByYearAndMonth(year, month).stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(expensesByYearAndMonth,
+                linkTo(methodOn(ExpenseController.class).getExpensesByYearAndMonth(year, month)).withSelfRel());
     }
 
     @PostMapping("/expenses")
-    Expense createExpense(@RequestBody Expense expense) {
-        return repository.save(expense);
+    ResponseEntity<?> createExpense(@RequestBody Expense expense) {
+        EntityModel<Expense> entityModel = assembler.toModel(repository.save(expense));
+        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
 
     @PutMapping("/expenses/{id}")
-    Expense updateExpense(@RequestBody Expense newExpense, @PathVariable Long id) {
-        return repository.findById(id)
+    ResponseEntity<?> updateExpense(@RequestBody Expense newExpense, @PathVariable Long id) {
+        Expense updatedExpense = repository.findById(id)
                 .map(expense -> {
                     expense.setName(newExpense.getName());
                     expense.setDescription(newExpense.getDescription());
@@ -59,10 +87,13 @@ public class ExpenseController {
                     newExpense.setId(id);
                     return repository.save(newExpense);
                 });
+        EntityModel<Expense> entityModel = assembler.toModel(updatedExpense);
+        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
 
     @DeleteMapping("/expenses/{id}")
-    void deleteExpense(@PathVariable Long id) {
+    ResponseEntity<?> deleteExpense(@PathVariable Long id) {
         repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
